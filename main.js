2a26467dc1c9f371d4e91d608f6bd82cb5431c7a
Dawid Domaradzki <dawid-kun@wp.pl>
2020-02-17 22:36:31 +0100

    nav adding

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log(`Dzien dobry`);
+
+const jstest = document.querySelector('.description__object--js');
+
+console.log(jstest);
+console.log(jstest.innerHTML);
+
+jstest.innerHTML = "teraz nadpisany tekst";
